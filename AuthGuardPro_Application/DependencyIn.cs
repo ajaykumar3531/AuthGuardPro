793ba23535a9106629f95727e0707bf328116fcd
AjayKumarYechuru <AjayKumarYechuru@WSIN63>
2024-10-24 16:59:40 +0530

    Add project files.

---
+++ AuthGuardPro_Application/DependencyIn.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthGuardPro_Application
+{
+    public static class DependencyIn
+    {
+        public static IServiceCollection ApplicationDI(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
---
+++ AuthGuardPro_Domain/DependencyIn.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthGuardPro_Core
+{
+    public static class DependencyIn
+    {
+        public static IServiceCollection CoreDI(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
